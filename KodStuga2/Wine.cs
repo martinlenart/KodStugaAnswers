ad3f64de761d97b21481a4879659488646d6b46a
Martin Lenart <martin_lenart@icloud.com>
2022-12-05 19:37:21 +0100

    updated

---
+++ KodStuga2/Wine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodStuga2
+{
+    public enum GrapeVariants
+    {
+        CabernetSauvignon, PinotNoir, Corvina, Shiraz, Merlot, Chablis,
+        Riesling, Tempranillo
+    }
+    public enum GrapeRegions
+    {
+        Bordeaux, Burgundy, Veneto, Piedmonte, RiberaDelDuero,
+        NapaValley, Puglia, Pfalz
+    }
+    public struct Wine
+    {
+        public int? Year;                   // null = undefined
+        public string Name;
+        public GrapeVariants Grape;
+        public GrapeRegions Region;
+
+        /// <summary>
+        /// Creates a string representing the content of the Wine struct
+        /// </summary>
+        /// <returns>string that can be printed out using Console.WriteLine</returns>
+        public override string ToString()
+        {
+            //Your code
+            return $"Wine {Year} {Name} is made of {Grape} from {Region}";
+        }
+    }
+}
---
+++ KodStuga2/WineCellar.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodStuga2
+{
+    public class WineCellar : IWineCellar
+    {
+        public const int maxNrBottles = 20;
+        Wine[] myCellar = new Wine[maxNrBottles];
+
+        public bool InsertWine(Wine wine)
+        {
+            //Your code
+            for (int i = 0; i < myCellar.Length; i++)
+            {
+                if (myCellar[i].Year == null)
+                {
+                    myCellar[i].Year = wine.Year;
+                    myCellar[i].Name = wine.Name;
+                    myCellar[i].Region = wine.Region;
+                    myCellar[i].Grape = wine.Grape;
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int NrOfBottles()
+        {
+            //Your code
+            int c = 0;
+            for (int i = 0; i < myCellar.Length; i++)
+            {
+                if (myCellar[i].Year != null)
+                {
+                    c++;
+                }
+            }
+            return c;
+        }
+
+        public int NrOfBottles(GrapeVariants grape)
+        {
+            //Your code
+            int c = 0;
+            for (int i = 0; i < myCellar.Length; i++)
+            {
+                if (myCellar[i].Year != null && myCellar[i].Grape == grape)
+                {
+                    c++;
+                }
+            }
+            return c;
+        }
+
+        public void PrintWines()
+        {
+            //Your code
+            foreach (var item in myCellar)
+            {
+                if (item.Year != null)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
